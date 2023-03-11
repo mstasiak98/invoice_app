9604f5531c298b5f5102f885410a42eb4ec8ee11
mstasiak98 <mikistasiak98@gmail.com>
2023-03-11 15:18:20 +0100

    fix payment date pipe

--- invoice_frontend/src/app/invoices/pipes/date-due.pipe.ts
+++ invoice_frontend/src/app/invoices/pipes/date-due.pipe.ts
@@ -12,19 +12,23 @@ export class DateDuePipe implements PipeTransform {
 
   transform(value: PaymentTerm, date: string): Date | string {
     const invDate = new Date(date);
+
     switch (value.name) {
       case Terms.PIA: return Terms.PIA;
       case Terms.CIA: return Terms.CIA;
       case Terms.COD: return Terms.COD;
       case Terms.EOM: return this.datePipe.transform(new Date(invDate.getFullYear(), invDate.getMonth() + 1, 0), 'mediumDate')!;
-      case Terms.NET7: return this.datePipe.transform(new Date(invDate.getDate() + 7), 'mediumDate')!;
-      case Terms.NET10: return this.datePipe.transform(new Date(invDate.getDate() + 10), 'mediumDate')!;
-      case Terms.NET15: return this.datePipe.transform(new Date(invDate.getDate() + 15), 'mediumDate')!;
-      case Terms.NET30: return this.datePipe.transform(new Date(invDate.getDate() + 30), 'mediumDate')!;
-      case Terms.NET60: return this.datePipe.transform(new Date(invDate.getDate() + 60), 'mediumDate')!;
+      case Terms.NET7: return this.datePipe.transform(DateDuePipe.getPaymentDate(invDate, 7), 'mediumDate')!;
+      case Terms.NET10: return this.datePipe.transform(DateDuePipe.getPaymentDate(invDate, 10), 'mediumDate')!;
+      case Terms.NET15: return this.datePipe.transform(DateDuePipe.getPaymentDate(invDate, 15), 'mediumDate')!;
+      case Terms.NET30: return this.datePipe.transform(DateDuePipe.getPaymentDate(invDate, 30), 'mediumDate')!;
+      case Terms.NET60: return this.datePipe.transform(DateDuePipe.getPaymentDate(invDate, 60), 'mediumDate')!;
       default: return 'Not defined'
     }
+  }
 
+  private static getPaymentDate(date: Date, offset: number): Date {
+    return new Date(`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate() + offset}`)
   }
 
 }
